89bea9ba9b5185d4aedd6c14eeb6feae6eef773a
Juanma Cervera <juanmacervera@gmail.com>
2015-04-17 09:43:00 +0200

    Subscribem. Sign up

---
+++ app/controllers/subscribem/accounts_controller.rb
@@ -0,0 +1,22 @@
+require_dependency "subscribem/application_controller"
+
+module Subscribem
+  class AccountsController < ApplicationController
+
+  	def new
+  		@account = Account.new
+  	end
+
+  	def create
+  		@accounts = Account.create(account_params)
+			flash[:notice] = 'Your account has been successfully created.'
+			redirect_to subscribem.root_url
+  	end
+
+  	private
+  	def account_params
+  		params.require(:account).permit(:name)
+  	end
+
+  end
+end
